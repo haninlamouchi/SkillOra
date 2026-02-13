bed68c38fc436af85f62f2fb0b2ec0b175d58092
Malek Dabbek <malekfathidabbek@gmail.com>
2026-02-13 05:42:37 +0100

    gestion_user back_office

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,25 @@ class AdminController extends AbstractController
     public function dashboard(): Response
     {
         return $this->render('backoffice/dashboard.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+
+    #[Route('/users/role/{role}', name: 'app_admin_users_by_role')]
+    public function usersByRole(string $role, UserRepository $userRepository): Response
+    {
+        // Valider que le rôle est correct
+        if (!in_array($role, ['responsable_club', 'etudiant', 'admin'])) {
+            throw $this->createNotFoundException('Invalid role');
+        }
+
+        // Récupérer les utilisateurs par rôle
+        $users = $userRepository->findBy(['role' => $role]);
+
+        // ✅ CORRECTION ICI : Pointer vers backoffice/users_list.html.twig
+        return $this->render('backoffice/users_list.html.twig', [
+            'users' => $users,
+            'role' => $role,
             'user' => $this->getUser(),
         ]);
     }
